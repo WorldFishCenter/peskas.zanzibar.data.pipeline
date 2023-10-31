9438536b6069d890bb0f41b6657a5b69680f4914
langbart <lorenzo.longobardi@gmail.com>
2023-10-31 14:00:46 +0100

    fix pars calls

--- R/ingest-wcs-surveys.R
+++ R/ingest-wcs-surveys.R
@@ -44,16 +44,17 @@ ingest_wcs_surveys <- function(log_threshold = logger::DEBUG) {
   pars <- read_config()
 
   file_list <- retrieve_wcs_surveys(
-    prefix = pars$surveys$wcs_catch_survey$file_prefix,
+    prefix = pars$surveys$wcs_surveys$file_prefix,
     file_format = "csv",
     append_version = TRUE,
     url = "kf.kobotoolbox.org",
-    project_id = pars$surveys$wcs_catch_survey$asset_id,
-    username = pars$surveys$wcs_catch_survey$username,
-    psswd = pars$surveys$wcs_catch_survey$password,
+    project_id = pars$surveys$wcs_surveys$asset_id,
+    username = pars$surveys$wcs_surveys$username,
+    psswd = pars$surveys$wcs_surveys$password,
     encoding = "UTF-8"
   )
 
+  pars$
   logger::log_info("Uploading files to cloud...")
   # Iterate over multiple storage providers if there are more than one
   purrr::map(pars$storage, ~ upload_cloud_file(file_list, .$key, .$options))
